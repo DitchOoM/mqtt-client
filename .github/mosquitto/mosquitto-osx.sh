d41da8a2b3fbe1138198c039d7948b2bb62739a2
Rahul Behera <thebehera@fb.com>
2022-01-05 01:06:40 -0800

    maybe this will work!

--- .github/mosquitto/mosquitto-osx.sh
+++ .github/mosquitto/mosquitto-osx.sh
@@ -1,2 +1,5 @@
 brew install mosquitto
-/usr/local/opt/mosquitto/sbin/mosquitto -c "${GITHUB_WORKSPACE}/.github/mosquitto/ditchoom_mqtt_test_server.conf"
+rm /usr/local/etc/mosquitto/mosquitto.conf
+cp "${GITHUB_WORKSPACE}/.github/mosquitto/ditchoom_mqtt_test_server.conf" /usr/local/etc/mosquitto/mosquitto.conf
+chmod 755 /usr/local/etc/mosquitto/mosquitto.conf
+/usr/local/opt/mosquitto/sbin/mosquitto -d -c "${GITHUB_WORKSPACE}/.github/mosquitto/ditchoom_mqtt_test_server.conf"
